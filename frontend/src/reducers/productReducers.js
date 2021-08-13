dc3327961f241e03d1d6003b28ee802301012736
Osman Can BİLGE <ocbilge@mikrokom.com>
2021-08-13 11:04:56 +0300

    add product screen to redux

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,7 @@
 import {
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -23,4 +26,23 @@ const productListReducer = (
   }
 };
 
-export { productListReducer };
+const productDetailsReducer = (
+  state = { loading: true, product: {} },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export { productListReducer, productDetailsReducer };
