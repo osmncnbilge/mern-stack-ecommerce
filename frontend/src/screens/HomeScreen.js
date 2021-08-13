78086a9ac025646b621fc0b4b0eeff54e620d89b
Osman Can BİLGE <ocbilge@mikrokom.com>
2021-08-13 11:30:28 +0300

    home screen updated

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
 import MessageBox from "../components/MessageBox";
 import LoadingBox from "../components/LoadingBox";
